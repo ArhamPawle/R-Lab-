6b4a5367f7bececa28285b60e084f412936eb493
ArhamPawle <67964537+ArhamPawle@users.noreply.github.com>
2022-02-16 10:57:35 +0530

    Add files via upload

---
+++ Exp1.r
@@ -0,0 +1,62 @@
+#Variable Assignment
+x = -3
+y <- 9
+z <<- 7
+5 -> a
+6 -> b
+
+
+x
+y
+z
+a
+b
+
+
+#PEMDAS EXPRESSIONS
+9 + 8 / 5 ^ 2
+9 + 8 / (5 ^ 2)
+9 + (8 / 5) ^ 2
+
+x + (y / 5) ^ z
+
+1 / 11
+options(digits = 3)
+1 / 11
+
+options(digits = 5)
+1 / 11
+
+22/7 # Regular Division
+22 %/% 7 # Integer Division
+22 %% 7 # Modulo (Remainder)
+
+
+#Mathematical Functions
+abs(x)
+sqrt(y)
+exp(z)
+log(y)
+log10(y)
+cos(z)
+
+#Infinite and NaN numbers
+1/0
+
+Inf - Inf
+
+#Console Functions
+ls()
+exists("z")
+rm(x)
+rm(y,z)
+ls()
+
+x = 5
+y = 7
+z = 9
+ls()
+
+rm(list = ls())
+
+ls()
